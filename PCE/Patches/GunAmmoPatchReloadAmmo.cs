7386305587894016c6256e1dc5cd8192babd9455
pdcook <qphysicscook@gmail.com>
2021-06-28 16:25:49 -0400

    patched bug (?) in GunAmmo.ReloadAmmo that caused the incorrect number of bullets to be drawn if the max ammo was modified while reloading

---
+++ PCE/Patches/GunAmmoPatchReloadAmmo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using PCE.MonoBehaviours;
+using PCE.RoundsEffects;
+using UnityEngine;
+using PCE.Extensions;
+
+
+namespace PCE.Patches
+{
+    // postfix to make sure ammo is drawn correctly if maxAmmo is modified while the player is reloading
+    [Serializable]
+    [HarmonyPatch(typeof(GunAmmo), "ReloadAmmo")]
+    class GunAmmoPatchReloadAmmo
+    {
+        private static void Postfix(GunAmmo __instance)
+        {
+            __instance.ReDrawTotalBullets();
+        }
+    }
+}
