54d771b7738089837c061f49cb4313457c1f57ef
pdcook <qphysicscook@gmail.com>
2021-07-05 21:25:03 -0400

    new cards

---
+++ PCE/Cards/RetreatCard.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnboundLib.Cards;
+using UnboundLib;
+using UnityEngine;
+using PCE.MonoBehaviours;
+
+namespace PCE.Cards
+{
+    public class RetreatCard : CustomCard
+    {
+
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
+        {
+            cardInfo.allowMultiple = false;
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            HealthBasedEffect effect = player.gameObject.AddComponent<HealthBasedEffect>();
+            effect.blockModifier.additionalBlocks_add = 1;
+            effect.blockModifier.cdMultiplier_mult = 0.5f;
+            effect.characterStatModifiersModifier.movementSpeed_mult = 1.5f;
+            effect.SetPercThresholdMax(0.2f);
+            effect.SetColor(Color.blue);
+        }
+        public override void OnRemoveCard()
+        {
+        }
+
+        protected override string GetTitle()
+        {
+            return "Retreat";
+        }
+        protected override string GetDescription()
+        {
+            return "Get boosted defense stats when below 20% of your max HP.\nWhen active:";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat
+                {
+                positive = true,
+                stat = "Additional Blocks",
+                amount = "+1",
+                simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+                new CardInfoStat
+                {
+                positive = true,
+                stat = "Block Cooldown",
+                amount = "-50%",
+                simepleAmount = CardInfoStat.SimpleAmount.aLotLower
+                },
+                new CardInfoStat
+                {
+                positive = true,
+                stat = "Movement Speed",
+                amount = "+50%",
+                simepleAmount = CardInfoStat.SimpleAmount.aLotOf
+                },
+
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.DefensiveBlue;
+        }
+    }
+}
---
+++ PCE/Cards/SuperBallCard.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnboundLib.Cards;
+using UnityEngine;
+using PCE;
+using UnboundLib;
+using PCE.Extensions;
+using PCE.RoundsEffects;
+using System.Linq;
+
+
+namespace PCE.Cards
+{
+    public class SuperBallCard : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
+        {
+            cardInfo.allowMultiple = true;
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            // get the screenEdge (with screenEdgeBounce component) from the TargetBounce card
+            CardInfo[] cards = global::CardChoice.instance.cards;
+            CardInfo targetBounceCard = (new List<CardInfo>(cards)).Where(card => card.gameObject.name == "TargetBounce").ToList()[0];
+            Gun targetBounceGun = targetBounceCard.GetComponent<Gun>();
+            ObjectsToSpawn screenEdgeToSpawn = (new List<ObjectsToSpawn>(targetBounceGun.objectsToSpawn)).Where(objectToSpawn => objectToSpawn.AddToProjectile.GetComponent<ScreenEdgeBounce>() != null).ToList()[0];
+
+
+            List<ObjectsToSpawn> objectsToSpawn = gun.objectsToSpawn.ToList();
+            objectsToSpawn.Add(screenEdgeToSpawn);
+            gun.objectsToSpawn = objectsToSpawn.ToArray();
+
+            gun.speedMOnBounce = 1.25f;
+            gun.dmgMOnBounce = 0.75f;
+            gun.reflects += 5;
+
+        }
+        public override void OnRemoveCard()
+        {
+        }
+
+        protected override string GetTitle()
+        {
+            return "Super Ball";
+        }
+        protected override string GetDescription()
+        {
+            return "Bullets accelerate after each bounce";
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat
+                {
+                positive = true,
+                stat = "Bounces",
+                amount = "+5",
+                simepleAmount = CardInfoStat.SimpleAmount.aHugeAmountOf
+                },
+                new CardInfoStat
+                {
+                    positive = false,
+                    stat = "DMG per bounce",
+                    amount = "-25%",
+                    simepleAmount = CardInfoStat.SimpleAmount.lower
+                }
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.DestructiveRed;
+        }
+    }
+}
