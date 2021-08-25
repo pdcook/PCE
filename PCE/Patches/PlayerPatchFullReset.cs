a5b3ac276c879a2df89c5e7b6449255d8303f447
tddebart <tddebart@gmail.com>
2021-08-25 19:24:21 +0200

    fixed effects not resetting

---
+++ PCE/Patches/PlayerPatchFullReset.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HarmonyLib;
+using PCE.Extensions;
+
+namespace PCE.Patches
+{
+    // patch to reset cards and effects
+    [Serializable]
+    [HarmonyPatch(typeof(Player), "FullReset")]
+    class PlayerPatchFullReset
+    {
+        private static void Prefix(Player __instance)
+        {
+            CustomEffects.DestroyAllEffects(__instance.gameObject);
+        }
+    }
+}
